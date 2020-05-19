2c9f534a918a80f55a24e55ccee58054c885d333
CaMMelo <cmelo.marcelo.pereira@gmail.com>
2020-05-19 07:54:46 -0300

    database defaults

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,7 +4,6 @@ const path = require("path");
 // make sure all controllers are imported automatically
 module.exports = (app) => {
   const dir = path.join(__dirname);
-  let controllers = [];
   fs.readdirSync(dir)
     .filter((dir) => !/^index.js$/.test(dir))  // remove index.js file
     .forEach((file) => {
