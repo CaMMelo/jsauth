630511ea4f59e7fca22def06dee6bba2868ef407
CaMMelo <cmelo.marcelo.pereira@gmail.com>
2020-05-25 23:25:36 -0300

    remove useless roles endpoints

--- server/controller/roles/index.js
+++ server/controller/roles/index.js
@@ -2,22 +2,12 @@ const express = require("express");
 const Authenticated = require("../../middleware/authenticated");
 const Protected = require("../../middleware/protected");
 const RequestHandler = require("../../utils/request_handler");
-const Create = require("./service/create");
 const Activate = require("./service/activate");
 const Deactivate = require("./service/deactivate");
-const Delete = require("./service/delete");
 const app = express();
 
 app.use(express.json());
 app.use(Authenticated, Protected(["ADMIN"]));
-
-app.post(
-  "/",
-  RequestHandler((req) => {
-    const { names } = req.body;
-    return Create(names);
-  })
-);
 
 app.patch(
   "/activate",
@@ -34,14 +24,6 @@ app.patch(
     return Deactivate(ids);
   })
 );
-
-app.delete(
-  "/:id/delete",
-  RequestHandler((req) => {
-    const { id } = req.params;
-    return Delete(id);
-  })
-)
 
 module.exports = (parent) => {
   parent.use("/roles", app);
